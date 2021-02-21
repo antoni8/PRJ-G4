acbba54d6341c77bd0125840fda281cdaf514769
acuevasdela14713 <acuevas3@hotmail.es>
2021-02-21 20:57:31 +0100

    Funcio de comprovacio

--- usuari.php
+++ usuari.php
@@ -7,8 +7,7 @@ class Usuari extends DB{
     $this->db = getDB();
   }
 
-  public function login($correu,$contrasenya){
-    // Comprovar si l'usuari existeix
+  public function comprovar($correu){
     $comprovacio = "select * from usuari where correu='$correu'";
     $resultatComprovacio = $this->db->query($comprovacio);
     $files = $resultatComprovacio->num_rows;
@@ -16,14 +15,22 @@ class Usuari extends DB{
       return "No existeix un usuari amb aquest correu assigant";
       exit;
     }
+  }
 
+  public function login($correu,$contrasenya){
+    comprovar($correu);
     $contrasenya = md5($contrasenya);
     $conBD = "select idUsuari from usuari where correu='$correu' and contrasenya='$contrasenya'";
     $resultatConBD = $this->db->query($conBD);
-    $files = $resultatComprovacio->num_rows;
   }
 
+  public function registrar($nom,$usuari,$correu,$contrasenya){
+    comprovar($correu);
 
+    $contrasenya = md5($contrasenya);
+    $nou = "insert into usuaris ";
+    $resultatNou = $this->db->query($nou);
+  }
 
 }
 
