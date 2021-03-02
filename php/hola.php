ab51f22a77b5f2af572155e77a66ebca53d5dbee
Adrian <acuevas3@hotmail.es>
2021-03-02 18:51:42 +0100

    hla

--- php/hola.php
+++ php/hola.php
@@ -10,7 +10,7 @@ include 'classes/usuari.php';
 
 $hola = new Usuari();
 
-$hola->registrar("41622494S","adria","adrian@hola.com","hola","12345678k");
+//$hola->registrar("41622494S","adria","adrian@hola.com","hola","12345678k");
 
 $hola->login("adrian@hola.com","hola");
 
