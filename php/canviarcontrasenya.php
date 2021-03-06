d715873fc42e76895bf1f7421c1cd39df12942a6
acuevasdela14713 <acuevas3@hotmail.es>
2021-03-06 20:42:39 +0100

    cometes

--- php/canviarcontrasenya.php
+++ php/canviarcontrasenya.php
@@ -8,10 +8,22 @@ echo "<html>
 echo "<form action='' method='get'>";
 echo "<input type='password' name='password'><input type='submit' value='VERIFICA'></form>";
 if (isset($_GET["password"])){
-  echo "<form action='' method='post'>";
+  echo "<form action='<?php echo $_SERVER[PHP_SELF']; ?>' method='post'>";
   echo "Indica la nova contrasenya:\n<input type='password' name='contranovao'><br>";
   echo "Verifica la nova contraseya:\n<input type='password' name='contranovar'><br>";
   echo "<input type='submit' value='CANVIA'></form>";
 }
 echo "</body></html>";
+
+include 'classes/usuari.php';
+
+if (isset($_GET)) {
+    $contrasenya = $_GET['contranovar'];
+    $dni = $_SESSION['id'];
+
+    $canviar = new Usuari();
+    $canviar->canviaContra($dni,$contrasenya);
+
+}
+
 ?>
