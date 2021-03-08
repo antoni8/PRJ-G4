2e3f425606880522c32094d5a9aaeda447909466
Adrian <acuevas3@hotmail.es>
2021-03-08 16:43:17 +0100

    Comprovacio

--- php/canviarcontrasenya.php
+++ php/canviarcontrasenya.php
@@ -8,7 +8,7 @@ echo "<html>
 <body>";
 echo "<form action='' method='get'>";
 echo "<input type='password' name='password'><input type='submit' value='VERIFICA'></form>";
-if (isset($_GET["password"]&coincideix_amb sa base de dades)){
+if (isset($_GET["password"])&(($comprovar->canviaContra($dni,$contrasenya) == false))){
   echo "<form action='<?php echo $_SERVER[PHP_SELF]; ?>' method='post'>";
   echo "Indica la nova contrasenya:\n<input type='password' name='contranovao'><br>";
   echo "Verifica la nova contraseya:\n<input type='password' name='contranovar'><br>";
@@ -19,8 +19,12 @@ echo "</body></html>";
 include 'classes/usuari.php';
 
 if (isset($_GET)) {
-    $contrasenya = $_GET['contranovar'];
-    $dni = $_SESSION['id'];
+  $comprovar = new Usuari();
+  $dni = $_SESSION['id'];
+}
+
+if (isset($_POST)) {
+    $contrasenya = $_POST['contranovar'];
 
     $canviar = new Usuari();
     $canviar->canviaContra($dni,$contrasenya);
